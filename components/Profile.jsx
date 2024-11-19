d43c4a31a80b0e484d9a91d86281493ddbcd5e71
Marianne <marianne@webcoda.com.au>
2024-11-20 02:33:19 +0800

    Update Profile.jsx

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -11,7 +11,7 @@ const Profile = ({
   return (
     <section className='w-full px-4'>
       <h1 className='head_text text-left'>
-        <span className='blue_gradient'>{name}'s Profile</span>
+        <span className='blue_gradient'>{name} Profile</span>
       </h1>
       <p className='desc'>{desc}</p>
       <div className='my-10 prompt_layout'>
